4eaf0dc9f1eea0c058e96f282d1b5cc43a2c2158
znanx <rifando.p.p@gmail.com>
2025-12-05 13:53:55 +0700

    feat(add): iqc

---
+++ plugins/converter/iqc.js
@@ -0,0 +1,28 @@
+module.exports = {
+   help: ['iqc'],
+   use: 'text',
+   tags: 'converter',
+   run: async (m, {
+      conn,
+      usedPrefix,
+      command,
+      text,
+      Func
+   }) => {
+      try {
+         if (!text) throw Func.example(usedPrefix, command, 'moonbot')
+         if (text.length > 50) throw Func.texted('bold', '🚩 Text is too long, max 50 characters.')
+         let old = new Date()
+         conn.sendReact(m.chat, '🕒', m.key)
+         const json = await Api.get('/iqc', {
+            text: text
+         })
+         if (!json.status) throw Func.jsonFormat(json)
+         conn.sendFile(m.chat, json.data.url, Func.filename('jpg'), `🍟 *Process* : ${((new Date - old) * 1)} ms`, m)
+      } catch (e) {
+         throw Func.jsonFormat(e)
+      }
+   },
+   limit: 1,
+   error: false
+}
